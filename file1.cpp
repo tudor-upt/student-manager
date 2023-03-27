98fd0a53524849b105bdaf9905dc66c3f1473777
iasminam <iasmina.martinovici@student.upt.ro>
2023-03-27 10:49:35 +0300

    dam comit la bataie

--- file1.cpp
+++ file1.cpp
@@ -3,3 +3,12 @@
 int main() {
     std::cout << "Coco nu stie sa foloseasca git\n";
 }
+
+@Override
+public void start(Stage primaryStage) throws Exception {
+   Parent root = FXMLLoader.load(getClass().getResource("fxml/view-students.fxml"));
+   primaryStage.setTitle("Student Manager");
+   primaryStage.setScene(new Scene(root, 600, 400));
+   primaryStage.show();
+}
+
